44f4768b36df7fcd70b84da23328ac21cc70d3cc
Maximilian <erik.maximilian.forsman@nav.no>
2018-12-13 13:27:01 +0100

    Wiremock instead of mockclientengine

--- src/id/IdGateway.kt
+++ src/id/IdGateway.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.http.URLBuilder
 import io.ktor.http.Url
+import io.ktor.http.fullPath
 import io.ktor.http.takeFrom
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -16,10 +17,10 @@ class IdGateway(
     val logger: Logger = LoggerFactory.getLogger("IdGateway")
 
     suspend fun getId() : String {
-        val url = URLBuilder().takeFrom(baseUri).path("foo").buildString()
+        val url = URLBuilder().takeFrom(baseUri).path(baseUri.fullPath, "foo").buildString()
         logger.info("Requesting url '{}'", url)
-        val response = httpClient.get<String>(url)
+        val response = httpClient.get<IdResponse>(url)
         logger.info("id='{}'", response)
-        return response
+        return response.id
     }
 }
--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,72 +1,79 @@
 package no.nav.pleiepenger.api
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.typesafe.config.ConfigFactory
-import io.ktor.client.*
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import io.ktor.http.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import io.ktor.client.engine.mock.*
-import kotlinx.coroutines.io.*
-import io.ktor.config.HoconApplicationConfig
+import io.ktor.config.MapApplicationConfig
 import no.nav.pleiepenger.api.general.jackson.configureObjectMapper
 import no.nav.pleiepenger.api.id.IdResponse
+import org.junit.AfterClass
 import org.junit.BeforeClass
 
 class ApplicationTest {
 
     private val objectMapper = configureObjectMapper()
 
+
     private companion object {
+
+        val wireMockServer: WireMockServer = WireMockServer(WireMockConfiguration.options().dynamicPort())
+
+        init {
+            wireMockServer.start()
+        }
+
+
         val engine = TestApplicationEngine(createTestEnvironment {
-            config = HoconApplicationConfig(ConfigFactory.load("application-test.conf"))
+            config = MapApplicationConfig().apply {
+                put("nav.cors.addresses", listOf("http://localhost:8888"))
+                put("nav.gateways.idGateway.baseUrl",
+                    URLBuilder().takeFrom(wireMockServer.baseUrl()).path("id-gateway").build().toString()
+                )
+            }
             module {
-                pleiepengesoknadapi(HttpClient(MockEngine {
-                    if (url.encodedPath == "/foo") {
-                        MockHttpResponse(
-                            call,
-                            HttpStatusCode.OK,
-                            ByteReadChannel("1234".toByteArray(Charsets.UTF_8)),
-                            headersOf()
-                        )
-                    } else {
-                        responseError(HttpStatusCode.NotFound, "Not Found ${url.encodedPath}")
-                    }
-                }) {
-                    expectSuccess = false
-                })
+                pleiepengesoknadapi()
             }
         })
 
-        @BeforeClass @JvmStatic fun setup(){
+
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
             engine.start(wait = false)
+            configureFor(wireMockServer.port())
+
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun tearDown() {
+            wireMockServer.stop()
         }
     }
 
 
     @Test
     fun getIdTest() {
-
+        stubFor(
+            get("/id-gateway/foo")
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"id\":\"1234\"}")
+                )
+        )
         with(engine) {
             with(handleRequest(HttpMethod.Get, "/id/12345678911") {
                 addHeader("accept", "application/json")
             }) {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals(IdResponse("1234"), objectMapper.readValue(response.content!!))
-            }
-        }
-    }
-
-
-    @Test
-    fun testRoo2t() {
-
-        with(engine) {
-            with(handleRequest(HttpMethod.Get, "/soker/1234") {
-                addHeader("accept", "application/json")
-            }) {
-                assertEquals(HttpStatusCode.OK, response.status())
-            }
+           }
         }
     }
 }
