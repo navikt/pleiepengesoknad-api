ed42ce30004580741c84156ff818adf3621ecfa3
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-06-13 16:13:19 +0200

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-import org.gradle.internal.impldep.org.fusesource.jansi.AnsiRenderer.test
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion = ext.get("ktorVersion").toString()
-val dusseldorfKtorVersion = "1.2.1.dbfcccb"
+val dusseldorfKtorVersion = "1.2.1.65ce328"
 val wiremockVersion = "2.19.0"
 
 val mainClass = "no.nav.helse.AppKt"
@@ -12,7 +11,7 @@ plugins {
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/dbfcccbfd1b79116749953dd73572c28688ae95c/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/65ce328364975f63e111891794b954b892001c76/gradle/dusseldorf-ktor.gradle.kts")
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31")
     }
@@ -24,16 +23,13 @@ dependencies {
     compile ( "no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
     compile ( "no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
     compile ( "no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
-
-    compile("io.ktor:ktor-auth-jwt:$ktorVersion")
+    compile ( "no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
     compile("io.ktor:ktor-locations:$ktorVersion")
-
 
     // Client
     compile ( "no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
-    compile ("io.ktor:ktor-client-json-jvm:$ktorVersion")
-    compile ("io.ktor:ktor-client-jackson:$ktorVersion")
-
+    compile ( "no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
+    
     // Test
     testCompile ("com.github.tomakehurst:wiremock:$wiremockVersion")
     testCompile("io.ktor:ktor-server-test-host:$ktorVersion") {
@@ -41,7 +37,6 @@ dependencies {
     }
 
     testCompile("org.skyscreamer:jsonassert:1.5.0")
-    testCompile ("com.nimbusds:oauth2-oidc-sdk:5.56")
 }
 
 repositories {
