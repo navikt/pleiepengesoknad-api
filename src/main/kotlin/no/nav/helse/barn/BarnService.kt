35b7e8f7d688b7f4599f7836dbed51a2d9a90d7b
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-08-23 12:25:18 +0200

    Feil ved oppslag på barn gir tom liste

--- src/main/kotlin/no/nav/helse/barn/BarnService.kt
+++ src/main/kotlin/no/nav/helse/barn/BarnService.kt
@@ -2,11 +2,15 @@ package no.nav.helse.barn
 
 import no.nav.helse.aktoer.NorskIdent
 import no.nav.helse.general.CallId
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class BarnService(
     private val barnGateway: BarnGateway
 ) {
     private companion object {
+        private val logger: Logger = LoggerFactory.getLogger(BarnService::class.java)
+
         private const val STATUS_DOED = "DØD"
         private const val DISKRESJONSKODE_STRENGT_FORTROLIG_ADRESSE = "SPSF"
         private const val DISKRESJONSKODE_FORTROLIG_ADRESSE = "SPFO"
@@ -20,9 +24,15 @@ class BarnService(
         norskIdent: NorskIdent,
         callId: CallId
     ) =
-        barnGateway.hentBarn(
-            norskIdent = norskIdent,
-            callId = callId)
-            .filter { it.status.toUpperCase() != STATUS_DOED }
-            .filter { !FILTRER_BORT_DISKRESJONSKODER.contains(it.diskresjonskode?.toUpperCase()) }
+        try {
+            barnGateway.hentBarn(
+                norskIdent = norskIdent,
+                callId = callId
+            )
+                .filter { it.status.toUpperCase() != STATUS_DOED }
+                .filter { !FILTRER_BORT_DISKRESJONSKODER.contains(it.diskresjonskode?.toUpperCase()) }
+        } catch (cause: Throwable) {
+            logger.error("Feil ved henting av barn, returnerer en tom liste", cause)
+            emptyList<Barn>()
+        }
 }
