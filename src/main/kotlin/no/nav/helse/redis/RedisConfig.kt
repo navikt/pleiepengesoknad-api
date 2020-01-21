975cb0fbb93bebba7b4d0d156f8a63ec6c781bfe
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-01-21 10:45:04 +0100

    Kryptering og redis

--- src/main/kotlin/no/nav/helse/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/helse/redis/RedisConfig.kt
@@ -3,13 +3,15 @@ package no.nav.helse.redis
 import io.ktor.util.KtorExperimentalAPI
 import io.lettuce.core.RedisClient
 import no.nav.helse.Configuration
+import org.slf4j.LoggerFactory
 
 class RedisConfig(private val redisConfigurationProperties: RedisConfigurationProperties) {
+    private val log = LoggerFactory.getLogger(RedisConfig::class.java)
 
     @KtorExperimentalAPI
     fun redisClient(configuration: Configuration): RedisClient {
         redisConfigurationProperties.startInMemoryRedisIfMocked()
-
+        log.info("redis://${configuration.getRedisHost()}:${configuration.getRedisPort()}")
         return RedisClient.create("redis://${configuration.getRedisHost()}:${configuration.getRedisPort()}")
     }
 
