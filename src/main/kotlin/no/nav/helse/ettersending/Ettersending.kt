f4e7e8dce203b5a5a521e6c9c00faa36cdfec68d
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-04-02 10:36:07 +0200

    La til JsonAlias, skal fjernes når frontend er oppdatert

--- src/main/kotlin/no/nav/helse/ettersending/Ettersending.kt
+++ src/main/kotlin/no/nav/helse/ettersending/Ettersending.kt
@@ -1,12 +1,15 @@
 package no.nav.helse.ettersending
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import java.net.URL
 
 data class Ettersending(
+    @JsonAlias("språk", "sprak") //TODO fjerne når frontend er oppdatert
     val sprak: String,
     val vedlegg: List<URL>,
     val harForstattRettigheterOgPlikter: Boolean,
     val harBekreftetOpplysninger: Boolean,
     val beskrivelse: String,
+    @JsonAlias("søknadstype", "soknadstype") //TODO fjerne når frontend er oppdatert
     val soknadstype: String
 )
