593b9ef922bb89d4f12c19c7d1f3c071ee0525e8
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-04-02 13:02:57 +0200

    Fjerner todo og jsonProperty

--- src/main/kotlin/no/nav/helse/ettersending/Ettersending.kt
+++ src/main/kotlin/no/nav/helse/ettersending/Ettersending.kt
@@ -1,18 +1,12 @@
 package no.nav.helse.ettersending
 
-import com.fasterxml.jackson.annotation.JsonAlias
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.net.URL
 
 data class Ettersending(
-    @JsonAlias("språk", "sprak") //TODO fjerne når frontend er oppdatert
     val sprak: String,
     val vedlegg: List<URL>,
-    @JsonAlias("harForståttRettigheterOgPlikter", "harForstattRettigheterOgPlikter") //TODO Fjernes når frontend er oppdatert
     val harForstattRettigheterOgPlikter: Boolean,
-    @JsonProperty("harBekreftetOpplysninger")
     val harBekreftetOpplysninger: Boolean,
     val beskrivelse: String,
-    @JsonAlias("søknadstype", "soknadstype") //TODO fjerne når frontend er oppdatert
     val soknadstype: String
 )
