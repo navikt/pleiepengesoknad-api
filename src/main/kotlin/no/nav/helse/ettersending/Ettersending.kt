0d218316f0da6f56ed2b8cd77d7323f3c418572d
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-04-02 11:42:08 +0200

    Jsonproperty for harBekreftet

--- src/main/kotlin/no/nav/helse/ettersending/Ettersending.kt
+++ src/main/kotlin/no/nav/helse/ettersending/Ettersending.kt
@@ -1,6 +1,7 @@
 package no.nav.helse.ettersending
 
 import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.net.URL
 
 data class Ettersending(
@@ -9,6 +10,7 @@ data class Ettersending(
     val vedlegg: List<URL>,
     @JsonAlias("harForståttRettigheterOgPlikter", "harForstattRettigheterOgPlikter") //TODO Fjernes når frontend er oppdatert
     val harForstattRettigheterOgPlikter: Boolean,
+    @JsonProperty("harBekreftetOpplysninger")
     val harBekreftetOpplysninger: Boolean,
     val beskrivelse: String,
     @JsonAlias("søknadstype", "soknadstype") //TODO fjerne når frontend er oppdatert
