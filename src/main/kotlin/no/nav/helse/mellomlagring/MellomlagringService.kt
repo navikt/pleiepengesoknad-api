47df56a71c1842c8bc88a7c26f864aed3f835da0
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-02-09 13:50:21 +0100

    Utvider funksjonalitet for å oppdatere mellomlagring uten å sette ny expiry. (#66)
    * Mellomlagrer søknad uten å oppdatere utgående dato.
    
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/helse/mellomlagring/MellomlagringService.kt
+++ src/main/kotlin/no/nav/helse/mellomlagring/MellomlagringService.kt
@@ -1,12 +1,15 @@
 package no.nav.helse.mellomlagring
 
-import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.*
 import no.nav.helse.redis.RedisStore
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.*
 
-class MellomlagringService @KtorExperimentalAPI constructor(private val redisStore: RedisStore, private val passphrase:String) {
+class MellomlagringService @KtorExperimentalAPI constructor(
+    private val redisStore: RedisStore,
+    private val passphrase: String
+) {
     private companion object {
         private val log: Logger = LoggerFactory.getLogger(MellomlagringService::class.java)
     }
@@ -17,20 +20,28 @@ class MellomlagringService @KtorExperimentalAPI constructor(private val redisSto
         fnr: String
     ): String? {
         val krypto = Krypto(passphrase, fnr)
-        val encrypted = redisStore.get(nøkkelPrefiks +fnr) ?: return null
+        val encrypted = redisStore.get(nøkkelPrefiks + fnr) ?: return null
         return krypto.decrypt(encrypted)
     }
 
     fun setMellomlagring(
         fnr: String,
-        midlertidigSøknad: String
-    ) {
-        val krypto = Krypto(passphrase, fnr)
-        val expirationDate = Calendar.getInstance().let {
+        midlertidigSøknad: String,
+        expirationDate: Date = Calendar.getInstance().let {
             it.add(Calendar.HOUR, 24)
             it.time
         }
-        redisStore.set(nøkkelPrefiks + fnr, krypto.encrypt(midlertidigSøknad),expirationDate)
+    ) {
+        val krypto = Krypto(passphrase, fnr)
+        redisStore.set(nøkkelPrefiks + fnr, krypto.encrypt(midlertidigSøknad), expirationDate)
+    }
+
+    fun updateMellomlagring(
+        fnr: String,
+        midlertidigSøknad: String
+    ) {
+        val krypto = Krypto(passphrase, fnr)
+        redisStore.update(nøkkelPrefiks + fnr, krypto.encrypt(midlertidigSøknad))
     }
 
     fun deleteMellomlagring(
