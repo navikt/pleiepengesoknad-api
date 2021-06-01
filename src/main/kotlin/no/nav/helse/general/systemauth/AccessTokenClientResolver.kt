1b8645e8ed55d36d7bf73b2f49a1b6d0f68302fc
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-01 12:08:27 +0200

    Legger til keyId på accessToken

--- src/main/kotlin/no/nav/helse/general/systemauth/AccessTokenClientResolver.kt
+++ src/main/kotlin/no/nav/helse/general/systemauth/AccessTokenClientResolver.kt
@@ -1,7 +1,9 @@
 package no.nav.helse.general.systemauth
 
+import com.nimbusds.jose.jwk.JWK
 import no.nav.helse.dusseldorf.ktor.auth.Client
 import no.nav.helse.dusseldorf.ktor.auth.PrivateKeyClient
+import no.nav.helse.dusseldorf.oauth2.client.DirectKeyId
 import no.nav.helse.dusseldorf.oauth2.client.FromCertificateHexThumbprint
 import no.nav.helse.dusseldorf.oauth2.client.FromJwk
 import no.nav.helse.dusseldorf.oauth2.client.SignedJwtAccessTokenClient
@@ -9,7 +11,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 internal class AccessTokenClientResolver(
-    clients : Map<String, Client>
+    clients: Map<String, Client>
 ) {
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(AccessTokenClientResolver::class.java)
@@ -20,11 +22,19 @@ internal class AccessTokenClientResolver(
         throw IllegalStateException("Client[$AZURE_V2_ALIAS] må være satt opp.")
     } as PrivateKeyClient
 
+    private val keyId = try {
+        val jwk = JWK.parse(azureV2Client.privateKeyJwk)
+        requireNotNull(jwk.keyID) { "Azure JWK inneholder ikke keyID." }
+        jwk.keyID
+    } catch (_: Throwable) {
+        throw IllegalArgumentException("Azure JWK på feil format.")
+    }
+
     private val azureV2AccessTokenClient = SignedJwtAccessTokenClient(
         clientId = azureV2Client.clientId(),
         tokenEndpoint = azureV2Client.tokenEndpoint(),
         privateKeyProvider = FromJwk(azureV2Client.privateKeyJwk),
-        keyIdProvider = FromCertificateHexThumbprint(azureV2Client.certificateHexThumbprint)
+        keyIdProvider = DirectKeyId(keyId)
     )
 
     internal fun accessTokenClient() = azureV2AccessTokenClient
