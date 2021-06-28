efe48f232412ece18c351325b0a2e63aa58f3c03
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-28 13:27:17 +0200

    Oppdaterer avhengigheter.
    Fjerner deprecated annotation
    
    Closes #118
    Closes #112
    Closes #111

--- src/main/kotlin/no/nav/helse/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/helse/redis/RedisConfig.kt
@@ -1,13 +1,11 @@
 package no.nav.helse.redis
 
-import io.ktor.util.*
 import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
 import java.time.Duration
 
 internal object RedisConfig {
 
-    @KtorExperimentalAPI
     internal fun redisClient(redisHost: String, redisPort: Int): RedisClient {
         return RedisClient.create(RedisURI(redisHost, redisPort, Duration.ofSeconds(60)))
     }
