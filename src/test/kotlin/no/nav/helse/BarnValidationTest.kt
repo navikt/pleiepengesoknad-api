74c26a8c9738e4c74b1817e94dd9271e8c602b55
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-02-01 15:09:52 +0100

    Legger til ÅrsakManglerIdentitetsnummer som støtter barn uten fnr. Oppdaterer validering og fikser utgått test.

--- src/test/kotlin/no/nav/helse/BarnValidationTest.kt
+++ src/test/kotlin/no/nav/helse/BarnValidationTest.kt
@@ -3,44 +3,61 @@ package no.nav.helse
 import no.nav.helse.dusseldorf.ktor.core.Violation
 import no.nav.helse.soknad.BarnDetaljer
 import no.nav.helse.soknad.validate
+import no.nav.helse.soknad.ÅrsakManglerIdentitetsnummer
 import java.time.LocalDate
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class BarnValidationTest {
+    val gyldigBarn = BarnDetaljer(
+        fødselsnummer = "02119970078",
+        fødselsdato = LocalDate.parse("2021-01-01"),
+        aktørId = "10000001",
+        navn = "Barnesen",
+        årsakManglerIdentitetsnummer = null
+    )
+
+    @Test
+    fun `Gyldig barn gir ingen feil`(){
+        gyldigBarn.validate().assertIngenFeil()
+    }
 
     @Test
     fun `Når AktørId settes som ID på barnet kreves hverken relasjon til barnet eller navn`() {
-        val barn = BarnDetaljer(
-            fødselsnummer = "02119970078",
-            fødselsdato = LocalDate.parse("2021-01-01"),
+        val barn = gyldigBarn.copy(
             aktørId = "10000001",
             navn = null
         )
-        barn.validate().assertFeilPaa()
+        barn.validate().assertIngenFeil()
     }
 
     @Test
-    fun `Når Fødselsnummer settes som ID på barnet kreves det relasjon`() {
-        val barn = BarnDetaljer(
-            fødselsnummer = "02119970078",
-            fødselsdato = null,
-            aktørId = null,
-            navn = null
-        )
-        barn.validate().assertFeilPaa()
-    }
-
-    @Test
-    fun `Skal gi feil dersom fødselsnummer ikke settes`() {
-        val barn = BarnDetaljer(
+    fun `Skal ikke gi feil selvom fødselsnummer er null så lenge fødselsdato og årsak er satt`() {
+        val barn = gyldigBarn.copy(
             fødselsnummer = null,
-            fødselsdato = null,
-            aktørId = null,
-            navn = null
+            fødselsdato = LocalDate.parse("2021-01-01"),
+            årsakManglerIdentitetsnummer = ÅrsakManglerIdentitetsnummer.NYFØDT
         )
-        barn.validate().assertFeilPaa(listOf("barn.fødselsnummer"))
+        barn.validate().assertIngenFeil()
+    }
+
+    @Test
+    fun `Skal gi feil dersom fødselsnummer ikke settes og man ikke har satt fødsesldato og årsak`() {
+        val barn = gyldigBarn.copy(
+            fødselsnummer = null,
+            årsakManglerIdentitetsnummer = null,
+            fødselsdato = null
+        )
+        barn.validate().assertFeilPaa(listOf("barn.fødselsdato", "barn.årsakManglerIdentitetsnummer"))
+    }
+
+    @Test
+    fun `Skal gi feil dersom fødselsdato er i fremtiden`() {
+        val barn = gyldigBarn.copy(
+            fødselsdato = LocalDate.now().plusDays(1)
+        )
+        barn.validate().assertFeilPaa(listOf("barn.fødselsdato"))
     }
 
 }
@@ -51,5 +68,8 @@ private fun MutableSet<Violation>.assertFeilPaa(parameterNames : List<String> = 
     forEach {
         assertTrue(parameterNames.contains(it.parameterName))
     }
+}
 
+private fun MutableSet<Violation>.assertIngenFeil() {
+    assertTrue(size == 0)
 }
