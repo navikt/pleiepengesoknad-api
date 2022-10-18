2b01de4ad2dd4bdd9423c17720352327fcb5c055
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-10-18 08:00:55 +0200

    Dependency, redis & gradle bumps (#282)
    * Dep + gradle bumps
    
    * Redis bump to fix openssl & zlib CVEs
    
    * K9-Format 7.0.2 + formatting
    
    * Bump oauth mock, fjernet ubrukt prop

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,67 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "3.2.0.2-259fbf4"
-val ktorVersion = ext.get("ktorVersion").toString()
-val k9FormatVersion = "5.8.8"
-val kafkaEmbeddedEnvVersion = ext.get("kafkaEmbeddedEnvVersion").toString()
-val kafkaVersion = ext.get("kafkaVersion").toString() // Alligned med version fra kafka-embedded-env
+val dusseldorfKtorVersion = "3.2.1.2-ce40a5b"
+val ktorVersion = "2.1.2"
+val k9FormatVersion = "7.0.2"
+val kafkaEmbeddedEnvVersion = "3.2.1"
+val kafkaVersion = "3.2.3"
 val fuelVersion = "2.3.1"
-val lettuceVersion = "6.1.8.RELEASE"
 val tokenSupportVersion = "2.1.1"
-val mockOauth2ServerVersion = "0.5.1"
+val mockOauth2ServerVersion = "0.5.4"
 
 val mainClass = "no.nav.helse.AppKt"
 
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-}
-
-buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/259fbf471e456e6de8ca226cf9e05f1185d71855/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
     // Server
-    implementation ( "no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
-    implementation ( "no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
-    implementation ( "no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
-    implementation ( "no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
-    implementation ( "no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
-    implementation("com.github.kittinunf.fuel:fuel-coroutines:$fuelVersion"){
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:$fuelVersion") {
         exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core")
     }
     implementation("io.ktor:ktor-server-cors:$ktorVersion")
 
     //K9-format
     implementation("no.nav.k9:soknad:$k9FormatVersion")
-    implementation ( "org.glassfish:jakarta.el:3.0.4")
+    implementation("org.glassfish:jakarta.el:3.0.4")
 
     // kafka
     implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
 
     // NAV
-    implementation ("no.nav.security:token-validation-ktor-v2:$tokenSupportVersion")
+    implementation("no.nav.security:token-validation-ktor-v2:$tokenSupportVersion")
 
     // Client
-    implementation ( "no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
-    implementation ( "no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
 
     // Test
-    testImplementation ("no.nav.security:mock-oauth2-server:$mockOauth2ServerVersion")
-    testImplementation ( "no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
-    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
-    testImplementation ("io.ktor:ktor-server-test-host:$ktorVersion") {
+    testImplementation("no.nav.security:mock-oauth2-server:$mockOauth2ServerVersion")
+    testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
+    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion") {
+        exclude(group = "org.apache.kafka", module = "kafka_2.13")
+    }
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
 
-    testImplementation ("org.skyscreamer:jsonassert:1.5.0")
+    testImplementation("org.skyscreamer:jsonassert:1.5.1")
     testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion") {
+        exclude(group="org.apache.kafka", module="kafka-clients")
+    }
+    testImplementation("io.mockk:mockk:1.13.1")
 }
 
 repositories {
@@ -107,7 +106,7 @@ tasks.withType<ShadowJar> {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "7.4.2"
+    gradleVersion = "7.5.1"
 }
 
 tasks.withType<Test> {
